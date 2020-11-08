ba1567c308728eafd546580864da8f48e0a39418
reysantana <58710601+reysantana@users.noreply.github.com>
2020-11-07 21:30:04 -0500

    added copy/paste

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -240,6 +240,13 @@ const createWindow = async () => {
       submenu: [{ role: 'reload' }, { label: 'custom reload' }],
     },
     {
+      label: "Edit",
+      submenu: [
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      ]
+    },
+    {
       label: 'About',
       click() {
         sendToClient(mainWindow, 'about_page');
